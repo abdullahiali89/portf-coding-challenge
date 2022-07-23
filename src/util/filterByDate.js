1f78d9b71b2b4552b39689bb8c3d3ee0a9e568ee
Abdullahi Ali <ali259abdullahi@gmail.com>
2022-07-23 22:01:44 +0100

    task complete

--- src/util/filterByDate.js
+++ src/util/filterByDate.js
@@ -4,15 +4,13 @@ export function filterByDate(startDate, endDate, data) {
   let eMonth = Number(endDate.split("-")[1]);
   let eYear = Number(endDate.split("-")[0]);
 
- 
-
   let newData = data.filter((data) => {
     let brewMonth = Number(data.first_brewed.split("/")[0]);
     let brewYear = Number(data.first_brewed.split("/")[1]);
 
     if (brewYear === undefined || isNaN(brewYear)) {
       brewYear = brewMonth;
-      brewMonth = 12;
+      brewMonth = 1;
     }
 
     return (
